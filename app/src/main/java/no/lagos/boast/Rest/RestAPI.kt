a42bee70b1b5f4636563a837f496a4b13147967a
Johannes D. Lagos <johanneslagos@gmail.com>
2015-11-19 00:37:06 +0100

    Added Query param to Retrofit

--- app/src/main/java/no/lagos/boast/Rest/RestAPI.kt
+++ app/src/main/java/no/lagos/boast/Rest/RestAPI.kt
@@ -1,11 +1,21 @@
 package no.lagos.boast.Rest
 
-import retrofit.http.GET
+import no.lagos.boast.Models.User
+import retrofit.Call
+import retrofit.http.*
 
 /**
  * Created by Johannes D. Lagos $today
  */
- interface RestAPI {
-    @GET("1/Login")
-    fun login(username: String, password: String)
+interface RestAPI {
+
+    @FormUrlEncoded
+    @Headers(
+            "X-Parse-Application-Id: dd6gnQ00Zav09LpwoLnMNRVHHBR7DgaBHBFepIcu",
+            "X-Parse-REST-API-Key: Fnx8qKrNeUCIludGzyzFfZ8D2WTHZ3NMVfRY3Ksi",
+            "X-Parse-Revocable-Session: 1"
+    )
+    @GET("Login")
+    fun login(@Query("username",encoded = true) username: String,
+              @Query("password", encoded = true) password: String):Call<User>
 }
